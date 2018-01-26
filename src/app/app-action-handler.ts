77ca786ffba68c1ff96137f4a18d2f286a9ad3c1
finnatinform <finn.zentgraf@inform-software.com>
2018-01-27 00:08:08 +0100

    added flux

---
+++ src/app/app-action-handler.ts
@@ -0,0 +1,10 @@
+import AppDispatcher = require('./app-dispatcher');
+import AppActionTypes = require('./app-action-types');
+
+class ActionHandler {
+
+}
+
+var AppActionHandler: ActionHandler = new ActionHandler();
+
+export = AppActionHandler;
---
+++ src/app/app-action-types.ts
@@ -0,0 +1,5 @@
+enum AppActionTypes {
+    
+}
+
+export = AppActionTypes ;
